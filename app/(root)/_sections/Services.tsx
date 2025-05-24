d314a8a0deb0ca109228c47994f465a735f9c47f
danielniebles <danielf.niebles@gmail.com>
2025-05-23 20:34:24 -0500

    feat: add shadcn button component and dependencies

--- app/(root)/_sections/Services.tsx
+++ app/(root)/_sections/Services.tsx
@@ -8,20 +8,19 @@ const Services = () => {
         <h2 className="heading !font-medium mb-8 text-center">
           ¿En qué <strong className="font-extrabold">podemos ayudarte?</strong>
         </h2>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        {SERVICES.map(({ title, icon }) => (
-          <div key={title} className="flex flex-col items-center">
-            <div className="relative w-36 h-36 mb-4">
-              <Image
-                src={icon} fill alt={title}
-                className="object-cover"
-              />
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          {SERVICES.map(({ title, icon }) => (
+            <div key={title} className="flex flex-col items-center">
+              <div className="relative w-36 h-36 mb-4">
+                <Image
+                  src={icon} fill alt={title}
+                  className="object-cover"
+                />
+              </div>
+              <span className="text-center w-64 text-white font-bold text-md">{title}</span>
             </div>
-            <span className="text-center w-64 text-white font-bold text-md">{title}</span>
-          </div>
-        ))}
-
+          ))}
+        </div>
       </div>
 
     </section>
